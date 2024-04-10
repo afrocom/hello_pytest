cd90de3b49fc06c7a81a41ab10585bd6fd5a0d2f
nyendwa <nyendwa@yahoo.com>
2024-04-10 22:06:31 +0200

    update simple function that uses socket to connect to ‘www.google.com’

--- src/myFunctions.py
+++ src/myFunctions.py
@@ -1,3 +1,4 @@
+import socket
 def sumOfInts(num1, num2):
 
     return num1 + num2
@@ -21,3 +22,27 @@ def print_ascii_stream(sentence):
     ascii_stream = [ord(char) for char in sentence]
     for ascii_code in ascii_stream:
         print(ascii_code, end=' ')
+
+def connect_to_google():
+    # Create a socket object
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Define the target host and port
+    target_host = 'www.google.com'
+    target_port = 80
+
+    # Connect to the server
+    client_socket.connect((target_host, target_port))
+
+    # Send some data
+    client_socket.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+    # Receive some data
+    response = client_socket.recv(4096)
+
+    print(response.decode())
+
+    # Close the socket
+    client_socket.close()
+
+
--- tests/tests_myFunctions.py
+++ tests/tests_myFunctions.py
@@ -25,3 +25,11 @@ def test_print_ascii_stream(capsys):
     myFunctions.print_ascii_stream(sentence)
     captured = capsys.readouterr()
     assert captured.out.strip() == "72 101 108 108 111 44 32 87 111 114 108 100 33"
+
+
+def test_connect_to_google():
+    myFunctions.connect_to_google()
+    assert True
+
+
+
